3ce9714641c3049f193ded6f3a5220a4d0b02b94
Xor0v0 <Xor0v0@163.com>
2025-02-25 00:48:42 +0800

    feat: support rwlock

--- src/metrics/cmap.rs
+++ src/metrics/cmap.rs
@@ -2,13 +2,13 @@ use anyhow::{anyhow, Result};
 use std::{
     collections::HashMap,
     fmt::Display,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
 
 /// Metrics: record the number of times a key is accessed
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Default for Metrics {
@@ -20,12 +20,12 @@ impl Default for Metrics {
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let count = data.entry(key.into()).or_insert(0);
         *count += 1;
         Ok::<_, anyhow::Error>(())
@@ -34,7 +34,7 @@ impl Metrics {
 
 impl Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.data.lock().map_err(|_| std::fmt::Error)?;
+        let data = self.data.read().map_err(|_| std::fmt::Error)?;
         for (key, value) in data.iter() {
             writeln!(f, "{}: {}", key, value)?;
         }
